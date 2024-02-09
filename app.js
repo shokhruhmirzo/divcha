e4e1e91763d17e26e9e1a5eea357e3dd6f5691a7
shokhruhmirzo <shaalimov923@gmail.com>
2024-02-09 21:07:37 +0500

    create

---
+++ app.js
@@ -0,0 +1,53 @@
+var div = document.querySelector('.div');
+
+function green(){
+    div.style.background = 'green'
+}
+
+function yellow(){
+    div.style.background = 'yellow'
+}
+
+function black(){
+    div.style.background = 'black'
+}
+
+function aqua(){
+    div.style.background = 'aqua'
+}
+
+function grey(){
+    div.style.background = 'grey'
+}
+
+function pink(){
+    div.style.background = 'palevioletred'
+}
+
+function ten(){
+    div.style.borderRadius= '10px'
+}
+
+function fifeteen(){
+    div.style.borderRadius= '15px'
+}
+
+function twentyfive(){
+    div.style.borderRadius= '20px'
+}
+
+function fourty(){
+    div.style.borderRadius= '25px'
+}
+
+function sixty(){
+    div.style.borderRadius= '40px'
+}
+
+function fifty(){
+    div.style.borderRadius= '50%'
+}
+
+function twf(){
+    div.style.borderRadius= '25%'
+}
